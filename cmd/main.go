8a3af65c641d509526d3811252a093a1dd711b72
talama014 <talama014@gmail.com>
2021-10-29 12:55:13 +0700

    add docker-compose.yml to create image: mysql ---- container: mysql:5.7 --- port:3306:3306

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"net/http"
 
+	"github.com/BookStore/pkg/book"
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func pingHandler(c *gin.Context) {
@@ -13,6 +17,17 @@ func pingHandler(c *gin.Context) {
 }
 func main() {
 	r := gin.Default()
+
+	dsn := "root:example@tcp(127.0.0.1:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&book.Book{}, &book.Authors{}, &book.Genres{}, &book.Publishers{})
+
 	r.GET("/ping", pingHandler)
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
